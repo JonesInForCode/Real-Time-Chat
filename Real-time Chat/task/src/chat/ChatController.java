5b0ff0eeff343f71297e8e76b6bb21b9d2af582e
TheMightyLoafer <nico.rjones@outlook.com>
2024-10-31 02:51:19 -0700

    initial proj upload

---
+++ Real-time Chat/task/src/chat/ChatController.java
@@ -0,0 +1,18 @@
+package chat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/chat")
+    public String chat() {
+        return "index";
+    }
+}
